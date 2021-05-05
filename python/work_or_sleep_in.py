840938b48602076fdf51e3de805287660937f19a
Zach Gleeson <zachary.a.gleeson@gmail.com>
2021-05-05 18:14:14 -0500

    added day_of_week.py and work_or_sleep_in homework

---
+++ python/work_or_sleep_in.py
@@ -0,0 +1,25 @@
+day = int(input('Day (0-6)? '))
+
+if day == 0:
+    print("Sleep in")
+
+elif day == 1:
+    print("Go to work")
+
+elif day == 2:
+    print("Go to work")
+
+elif day == 3:
+    print("Go to work")
+
+elif day == 4:
+    print("Go to work")
+
+elif day == 5:
+    print("Go to work")
+
+elif day == 6:
+    print("Sleep in")
+            
+else:
+    print("Not a valid answer. Please choose 0-6.")
